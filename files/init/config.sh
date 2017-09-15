cd12bb6d09433bcc83cb37f3c64190795150d408
Lev Gorenstein <lev@purdue.edu>
2017-09-15 02:39:16 -0400

    Comments for cluster-init config script

--- files/init/config.sh
+++ files/init/config.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+#
+# This file is sourced by all cloud-init scripts from ../scripts dir.
+# Main idea is to define common settings.
 
+# Where Cluster-Init files and scripts are.
+# Relative path ('..') works fine for testing and production.
+# Or could use production-quality absolute '/mnt/cluster-init', 
+# but may have to change for off-site testing.
 INIT=..
 FILES=$INIT/files
 SCRIPTS=$INIT/scripts/
+
+# And where the root of the destination filesystem is.
+# Things like '$INIT/../test' or '$HOME/test' are good for debugging.
+# Obviously should be set to "/" for production.
 ROOTDEST=$INIT/../test
 if [ ! -d $ROOTDEST ]; then
      mkdir -p $ROOTDEST
