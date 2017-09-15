107db9310817b28adb85faadfa1f6125946c39ad
Lev Gorenstein <lev@purdue.edu>
2017-09-15 01:07:15 -0400

    Meaningful names and inside comments are important

--- scripts/001_fix_login_defs.sh
+++ scripts/001_fix_login_defs.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Pull in config settings
 source ../files/init/config.sh
+
+# Our tweaked login.defs
 [ -d $ROOTDEST/etc ] || mkdir -p $ROOTDEST/etc 
 cp $FILES/etc/login.defs $ROOTDEST/etc/
