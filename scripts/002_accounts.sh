84bd9cc28c65080bd7c2d7331014442db26224ea
Jacob <nocturnametu@gmail.com>
2017-09-15 03:57:44 +0000

    Fixed the group errors and added usernames for lev and chuck :P

--- scripts/002_accounts.sh
+++ scripts/002_accounts.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # HHS
-useradd -u 1010 -g users -c "Chuck Schwarz" -p '$6$Db6M1nOH3bdT3fbu$GGbeO0eYSS7p6xMf21lIsjtqwD1pMzqxmyFVdtzJo6mQtpd5FrQ8FhyXDl24y46iNHKvNOcnvKcJL8oCQz352.'
-useradd -u 1011 -g users -c "Lev Gorenstein"   -p '$6$3.fHvkWXMVqm7JzP$20kNcI7cZHLaQu8/waPbo9NqiI3iAqiXtD4m.Za6/13Jogd7lmWr8GuNaCiljZNtx199ZI1q4dJzYwlVeIFqq0'
+useradd -u 1010 -g users -c "Chuck Schwarz" -p '$6$Db6M1nOH3bdT3fbu$GGbeO0eYSS7p6xMf21lIsjtqwD1pMzqxmyFVdtzJo6mQtpd5FrQ8FhyXDl24y46iNHKvNOcnvKcJL8oCQz352.' chuck
+useradd -u 1011 -g users -c "Lev Gorenstein"   -p '$6$3.fHvkWXMVqm7JzP$20kNcI7cZHLaQu8/waPbo9NqiI3iAqiXtD4m.Za6/13Jogd7lmWr8GuNaCiljZNtx199ZI1q4dJzYwlVeIFqq0' lev
 useradd -u 1012 -g users -c "Jeremy Meyer"     -p '$6$K5n/ABCN1OLeUulD$gvtFKmVQrXryU2WRThyu189hu82i9gHM.kJhBaFtMZgPaLIMYtgnSthrn9w.egXbTOEx/HBPVCSYAOdGoDo3e/'  jmeye90
 useradd -u 1013 -g users -c "Nick Nolte"       -p '$6$rHvJEV8cRsjY7DPx$2sH0T5KsnbPlVkxciCUvvSisvoT/lY2NwVUy/Oaw80lot3EEeYFrtWDA.Z7BmPWmYeugSZZPQhxGBanA7BT8h.'  budtomazi
 useradd -u 1014 -g users -c "Jacob Sharp"      -p '$6$p06BdgV7zkYyhRMW$tPxLJgZk3mSn5WaVdxCYlgvUc3iDNlowkfnZxRcoP.mz76uJVWgfaf7Jl8L650OrJ4E/EjLgQlAjK6/FcAzEI0'  sharp67
@@ -14,6 +14,6 @@ useradd -u 1019 -g users -c "Eduardo Wedekind" -p '$6$sOw2qNV9vRncEPa5$vb5tQ0ajc
 
 
 # Purdue students
-useradd -u 1002 -g users -G 1002 -c "Brandon Stewart"   -p '$6$5bUcs1.xNqOPARMZ$QHITxe5jGRCbIEVfAK81qnhtvIagbi99NmSZxRngrQKLyTdFC7jG4BES5uaZJtNVqqd4CYR8GtnSvX0cVy/B41' stewart
-useradd -u 1003 -g users -G 1003 -c "Austyn Cousins"    -p '$6$eK1ctmHkISlfnOZR$sz47opQycJoi/YKkAlrbInZdQ91sigc2smg6vwlFBE9buYqZ3DYKprp9Sk7CTfQZEQD5y.LrArI07mXI32.Vd0' acousin
-useradd -u 1004 -g users -G 1004 -c "Austin Horning"    -p '$6$CH97l36uxfGmnq02$fj2CctHkIeQ1Pl71ZM/ElrcoPYvgsfgvwQkaOnGsglu67LWh2tssAooJUCjF4.Ax8CXNtchHAehbd92b6.RqQ.' ahornin
+useradd -u 1002 -g users  -c "Brandon Stewart"   -p '$6$5bUcs1.xNqOPARMZ$QHITxe5jGRCbIEVfAK81qnhtvIagbi99NmSZxRngrQKLyTdFC7jG4BES5uaZJtNVqqd4CYR8GtnSvX0cVy/B41' stewart
+useradd -u 1003 -g users -c "Austyn Cousins"    -p '$6$eK1ctmHkISlfnOZR$sz47opQycJoi/YKkAlrbInZdQ91sigc2smg6vwlFBE9buYqZ3DYKprp9Sk7CTfQZEQD5y.LrArI07mXI32.Vd0' acousin
+useradd -u 1004 -g users -c "Austin Horning"    -p '$6$CH97l36uxfGmnq02$fj2CctHkIeQ1Pl71ZM/ElrcoPYvgsfgvwQkaOnGsglu67LWh2tssAooJUCjF4.Ax8CXNtchHAehbd92b6.RqQ.' ahornin
