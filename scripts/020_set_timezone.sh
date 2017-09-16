aeff9ebc842f7a7e824cc221237eccf3be5e2974
Lev Gorenstein <lev@purdue.edu>
2017-09-16 00:52:26 -0400

    Add timezone-setting script.

---
+++ scripts/020_set_timezone.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Set timezone to something American as opposed to default UTC.
+#
+# https://www.cyberciti.biz/faq/centos-linux-6-7-changing-timezone-command-line/
+
+# Pull in config settings... not really needed for this script, but ordnung!
+source 000_config.sh
+
+# TZ="America/Denver" 
+TZ="America/Indiana/Indianapolis"
+
+timedatectl set-timezone "$TZ"
