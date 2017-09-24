6311131d4228cfe5b8dabe429beffeaecf24bd2e
Lev Gorenstein <lev@purdue.edu>
2017-09-24 11:11:45 -0400

    Make /apps/cent7 symlink and folder

---
+++ scripts/050_make_apps_cent7.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Create /apps/cent7 for things we need to install
+
+# Pull in config settings
+source $(dirname "$BASH_SOURCE")/000_config.sh
+
+# Make it so! Create directory in shared filesystem, symlink from /apps
+mkdir -p $ROOTDEST/shared/apps/cent7
+ln -s -t $ROOTDEST  ${ROOTDEST%%/}/shared/apps 
