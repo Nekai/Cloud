7103543137d769fe137e8a09f8e257c731d21a2e
Lev Gorenstein <lev@purdue.edu>
2017-09-23 23:19:56 -0400

    Install some packages we think we need.

---
+++ scripts/100_install_packages.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Install packages we know we'll need
+
+# Pull in config settings... not really needed for this script, but ordnung!
+source $(dirname "$BASH_SOURCE")/000_config.sh
+
+# Where the list is. 
+PKGLIST=$SCRIPTS/packages/packagelist.txt
+
+# And go!
+# List is space- or newline-separated, with everything after any '#' 
+# considered a comment and ignored.
+yum install -y $(sed -e 's/#.*//' $PKGLIST)
