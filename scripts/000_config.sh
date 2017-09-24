1650757ce51b738965b9cb8d1391a24af29bfde4
Lev Gorenstein <lev@purdue.edu>
2017-09-24 12:35:57 -0400

    Harden the INIT directory determination.
    Original version ('..') expected your working directory to be where
    the script was.  So calling any of the scripts from other locations
    (e.g. 'run-parts ~/cloud/scripts') would have caused INIT variable
    to be set relative to *your* location (not *the script's* one),
    and thus not cause all sorts of problems for every next stage that
    relies on it.
    
    Using the '$(dirname "$BASH_SOURCE")/..' construct ties things relative
    to the place where currently executed script is...  which is exactly
    what we need.  More details in Wooledge FAQ at
    http://mywiki.wooledge.org/BashFAQ/028

--- scripts/000_config.sh
+++ scripts/000_config.sh
@@ -4,10 +4,10 @@
 # Main idea is to define common settings and virtually no direct action.
 
 # Where Cluster-Init files and scripts are.
-# Relative path ('..') works fine for testing and production.
-# Or could use production-quality absolute '/mnt/cluster-init', 
-# but may have to change for off-site testing.
-INIT=..
+# Relative path ('$(dirname "$BASH_SOURCE")/..') works fine for testing 
+# and production. Or could use production-quality absolute 
+# '/mnt/cluster-init', but may have to change for off-site testing.
+INIT=$(dirname "$BASH_SOURCE")/..
 FILES=$INIT/files
 SCRIPTS=$INIT/scripts/
 
