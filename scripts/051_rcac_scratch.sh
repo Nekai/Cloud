ace2be6bd7d6524707b48c0295fda3d2683bc627
Lev Gorenstein <lev@purdue.edu>
2017-09-25 22:22:24 -0400

    Make sure parent dir for scratches always exists
    Even on non-converged clusters, or when /shared is not present for
    whatever reason, etc.  This is not as mission-critical as homes, but
    still.

--- scripts/051_rcac_scratch.sh
+++ scripts/051_rcac_scratch.sh
@@ -6,10 +6,17 @@
 # Pull in config settings
 source $(dirname "$BASH_SOURCE")/000_config.sh
 
+# Make sure shared scratch exits and has proper wide-open permissions :)
+# Would have never done this in real life, but hey, we are not making a 
+# production scale system here, so it's ok among friends.
+SCRATCH_BASE=/shared/scratch
+[[ -d $SCRATCH_BASE ]] || mkdir -p $SCRATCH_BASE
+chmod 1777 $SCRATCH_BASE
+
 # Make top-level scratch link
-ln -s --force -t $ROOTDEST  ${ROOTDEST%%/}/shared/scratch
+ln -s --force -t $ROOTDEST  ${ROOTDEST%%/}/${SCRATCH_BASE##/}  # Careful with /
 
 # Create rcac-scratch.sh
-mkdir -p $ROOTDEST/etc/profile.d
+[[ -d /etc/profile.d ]] || mkdir -p $ROOTDEST/etc/profile.d
 cp $SCRIPTS/etc/profile.d/rcac-scratch.sh  $ROOTDEST/etc/profile.d
 cp $SCRIPTS/etc/profile.d/rcac-scratch.csh $ROOTDEST/etc/profile.d
